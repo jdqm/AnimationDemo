324c95ec7832b17c55b3f5a9c45ccab27aec4a1d
yangsheng <1923025333@qq.com>
2017-12-28 17:49:15 +0800

    add tween animation

--- app/src/main/java/com/jdqm/animationdemo/animation/TweenAnimationActivity.java
+++ app/src/main/java/com/jdqm/animationdemo/animation/TweenAnimationActivity.java
@@ -1,15 +1,31 @@
 package com.jdqm.animationdemo.animation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.jdqm.animationdemo.R;
 
 public class TweenAnimationActivity extends AppCompatActivity {
 
+    private View imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twenn_animation);
+        imageView = findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.music_animation);
+    }
+
+    public void start(View view) {
+        AnimationDrawable tweenAnimation = (AnimationDrawable) imageView.getBackground();
+        tweenAnimation.start();
+    }
+
+    public void stop(View view) {
+        AnimationDrawable tweenAnimation = (AnimationDrawable) imageView.getBackground();
+        tweenAnimation.stop();
     }
 }
