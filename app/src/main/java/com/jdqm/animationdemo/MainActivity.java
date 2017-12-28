866a4c1022c3fe1461c1596ec5f0f0b0ef1bea18
yangsheng <1923025333@qq.com>
2017-12-28 18:46:06 +0800

    add old activity transition

--- app/src/main/java/com/jdqm/animationdemo/MainActivity.java
+++ app/src/main/java/com/jdqm/animationdemo/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jdqm.animationdemo.animation.TranslationAnimationView;
 import com.jdqm.animationdemo.animation.TweenAnimationActivity;
 import com.jdqm.animationdemo.animation.ViewAnimationActivity;
 import com.jdqm.animationdemo.animator.PropertyAnimationActivity;
+import com.jdqm.animationdemo.transition.TransitionActivity;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -28,6 +30,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btnAnimator:
                 startActivity(new Intent(this, PropertyAnimationActivity.class));
+                overridePendingTransition(R.anim.activity_enter_anim, R.anim.activity_exit_anim);
+                break;
+            case R.id.btnTransition:
+                startActivity(new Intent(this, TransitionActivity.class));
+                overridePendingTransition(R.anim.activity_enter_anim, R.anim.activity_exit_anim);
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/jdqm/animationdemo/transition/TransitionActivity.java
@@ -0,0 +1,15 @@
+package com.jdqm.animationdemo.transition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jdqm.animationdemo.R;
+
+public class TransitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition);
+    }
+}
